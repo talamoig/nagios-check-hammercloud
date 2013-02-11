17470f4bce57d840ac90431d52a90b502d450ef8
Ivano Giuseppe Talamo <talamoig@cmsrm-ui02.roma1.infn.it>
2013-02-11 19:14:14 +0100

    code cleanup

--- check_hammercloud.py
+++ check_hammercloud.py
@@ -14,6 +14,12 @@ RET_WARN=1
 RET_CRIT=2
 RET_UNKN=3
 
+try:
+    site=sys.argv[1]
+except Exception:
+    sys.stderr.write("Please specify a site name as argument (eg. %s T2_IT_Rome)\n"%sys.argv[0])
+    sys.exit(RET_UNKN)
+    
 now=datetime.datetime.now()
 d=datetime.timedelta(1)
 yesterday=now-d
@@ -22,11 +28,6 @@ url='http://hammercloud.cern.ch/hc/app/cms/joberrors/?&cloud=&test=&template=&st
 response = urllib2.urlopen(url)
 html=response.read().split("\n")
 html=map(lambda x:x.strip(),html)
-try:
-    site=sys.argv[1]
-except Exception:
-    sys.stderr.write("Please specify a site name (eg. T2_IT_Rome)")
-    os.exit(unknown)
 res=filter(lambda x:x.find("<td>%s"%site)==0,html)
 if len(res)!=1:
     sys.exit(RET_UNKN)
@@ -37,14 +38,11 @@ m=r.match(res)
 if m:
     efficiency=m.group(1)
     efficiency=float(efficiency)
-    print "Hammer Cloud Efficiency: %s"%efficiency
+    print "HammerCloud Efficiency: %s"%efficiency
     if efficiency<CRITICAL:
         sys.exit(RET_CRIT)
     if efficiency<WARNING:
         sys.exit(RET_WARN)
     sys.exit(RET_OK)
 sys.exit(RET_UNKN)
-                                                    
-# should never be here
-# this neither
-# mhh
+        
